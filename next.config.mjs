16c2468c827b2a22f9c0725e4fe353dba8f00f91
Boyuan Gao <anti2moron@gmail.com>
2024-02-26 10:59:12 +0800

    fix: sync file script

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
     RCON_ADMIN_PASSWORD: process.env.RCON_ADMIN_PASSWORD,
     RCON_LOCATION: process.env.RCON_LOCATION,
   },
+  publicRuntimeConfig: {
+    SAVE_FILE_DIR: process.env.SAVE_FILE_DIR,
+    RCON_ADMIN_PASSWORD: process.env.RCON_ADMIN_PASSWORD,
+    RCON_LOCATION: process.env.RCON_LOCATION,
+  },
   experimental: {
     serverComponentsExternalPackages: ['lodash', 'fs', 'sqlite3', "jsonstream", "knex"],
   },
